acf6c867aeb96f7a42dac02b8ce8d0b0264b9d95
ryanascherr <ryanascherr@gmail.com>
2021-03-10 11:28:46 -0500

    Made a function to start the timer.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 var startButton = document.querySelector(".start-button");
+var timer = document.querySelector("#timer");
+
+var secondsLeft = 60;
 
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
     document.getElementsByTagName("header")[0].setAttribute("class", "hidden");
     document.getElementsByTagName("button")[0].setAttribute("class", "hidden");
+    startTimer();
 }
+
+function startTimer() {
+    // Sets timer
+    
+    timerFunction = setInterval(function() {
+        secondsLeft = 60;
+        secondsLeft--;
+        if (secondsLeft > 1) {
+            document.getElementById("timer").innerHTML = secondsLeft + " seconds remaining";
+        } else if (secondsLeft === 1) {
+            document.getElementById("timer").innerHTML = secondsLeft + " second remaining";
+        } else if (secondsLeft === 0) {
+            document.getElementById("timer").setAttribute("class", "hidden");
+        }
+    }, 1000);
+  }
