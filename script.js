0b20f844acce14e4beb9ea22a8d4478457acd87c
ryanascherr <ryanascherr@gmail.com>
2021-03-10 13:07:47 -0500

    Created second page and added several functions in js.

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 var startButton = document.querySelector(".start-button");
 var timer = document.querySelector("#timer");
+var pageTwo = document.querySelector("#whole-page-two");
+var questionText = document.querySelector(".question-page-two");
+
+var questions = ["Question 1:", "Question 2:", "Question 3:", "Question 4:", "Question 5:"]
+var answersOne = [];
+var answersTwo = [];
+var answersThree = [];
+var answersFour = [];
+var answersFive = [];
 
 var secondsLeft = 60;
+var page = 0;
 
 startButton.addEventListener("click", startQuiz);
 
 function startQuiz() {
+    hideMainPage();
+    startTimer();
+    page++;
+    displayPageOne();
+}
+
+function hideMainPage() {
     document.getElementsByTagName("header")[0].setAttribute("class", "hidden");
     document.getElementsByTagName("button")[0].setAttribute("class", "hidden");
-    startTimer();
 }
 
 function startTimer() {
-    // Sets timer
-    
     timerFunction = setInterval(function() {
-        secondsLeft = 60;
         secondsLeft--;
         if (secondsLeft > 1) {
             document.getElementById("timer").innerHTML = secondsLeft + " seconds remaining";
@@ -25,4 +38,9 @@ function startTimer() {
             document.getElementById("timer").setAttribute("class", "hidden");
         }
     }, 1000);
-  }
+}
+
+function displayPageOne() {
+    pageTwo.setAttribute("class", "display");
+    questionText.innerHTML = questions[page-1];
+}
