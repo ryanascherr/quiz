94a89e499469fe4dd1716eefc11ff28bf7c0605e
ryanascherr <ryanascherr@gmail.com>
2021-03-11 12:29:53 -0500

    Changed certain elements of JS to make program run smoother.

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ var introPage = document.querySelector("#intro-page");
 var questionPage = document.querySelector("#question-page");
 var scorePage = document.querySelector("#score-page");
 var questionText = document.querySelector("#question-text");
-var answerText = document.querySelectorAll(".answer-button");
 var clickable = document.querySelector(".clickable");
+var answerText = document.querySelectorAll(".answer-button");
 
-var questions = ["Inside which HTML element do we put the JavaScript?", 'What is the correct JavaScript syntax to change the content of the following HTML element:', 'How do you write "Hello World" in an alert box?', "How do you create a function in JavaScript?", "How to write an IF statement in JavaScript?"]
+var questions = [
+"Inside which HTML element do we put the JavaScript?",
+'What is the correct JavaScript syntax to change the content of the following HTML element:', 
+'How do you write "Hello World" in an alert box?', "How do you create a function in JavaScript?",
+"How to write an IF statement in JavaScript?"
+]
+
 var answers = {
-    pageOne: ["1: <script>", "2: JS", "3: javascript", "4: scripting"],
+    pageOne: ["1: <script>", "2: <JS>", "3: <javascript>", "4: <scripting>"],
     pageTwo: ["4:", "5:", "Correct Answer 2", "7:"],
     pageThree: ['1: msgBox("Hello World");', '2: msg("Hello World");', '3: alertBox("Hello World");', '4: alert("Hello World")'],
     pageFour: ["1: function = myFunction()", "2: function myFunction()", "3: function:myFunction()", "4: function(myFunction())"],
@@ -18,6 +24,7 @@ var answers = {
 
 var secondsLeft = 60;
 var pageNumber = 0;
+var points;
 
 startButton.addEventListener("click", startQuiz);
 
@@ -26,7 +33,10 @@ clickable.addEventListener("click", function(event) {
 
     if (element.matches(".answer-button")) {
         pageNumber++;
-        if (element.innerHTML.includes("1: script") || (element.innerHTML.includes("Correct Answer 2")) || (element.innerHTML.includes("Correct Answer 3"))) {
+        if (element.textContent.includes("1: <script>") ||
+        (element.textContent.includes()) ||
+        (element.textContent.includes('4: alert("Hello World")')))
+        {
             secondsLeft = secondsLeft;
         } else {
             secondsLeft = secondsLeft - 10;
@@ -48,8 +58,9 @@ function startTimer() {
             document.getElementById("timer").innerHTML = secondsLeft + " seconds remaining";
         } else if (secondsLeft === 1) {
             document.getElementById("timer").innerHTML = secondsLeft + " second remaining";
-        } else if (secondsLeft === 0) {
+        } else if (secondsLeft <= 0) {
             document.getElementById("timer").setAttribute("class", "hidden");
+            displayScorePage();
         }
     }, 1000);
 }
@@ -58,28 +69,29 @@ function displayIntroPage(){
     scorePage.setAttribute("class", "hidden");
     questionPage.setAttribute("class", "hidden");
     introPage.setAttribute("class", "display");
+    pageNumber = 0;
 }
 
 function displayQuestionPage(){
     scorePage.setAttribute("class", "hidden");
     introPage.setAttribute("class", "hidden");
     questionPage.setAttribute("class", "display");
-    questionText.innerHTML = questions[pageNumber-1];
+    questionText.textContent = questions[pageNumber-1];
     if (pageNumber === 1) {
         for (i = 0; i < 4; i++) {
-        answerText[i].innerHTML = answers.pageOne[i];}
+        answerText[i].textContent = answers.pageOne[i];}
     } else if (pageNumber === 2) {
         for (i = 0; i < 4; i++) {
-        answerText[i].innerHTML = answers.pageTwo[i];}
+        answerText[i].textContent = answers.pageTwo[i];}
     } else if (pageNumber === 3) {
         for (i = 0; i < 4; i++) {
-        answerText[i].innerHTML = answers.pageThree[i];}
+        answerText[i].textContent = answers.pageThree[i];}
     } else if (pageNumber === 4) {
         for (i = 0; i < 4; i++) {
-        answerText[i].innerHTML = answers.pageFour[i];}
+        answerText[i].textContent = answers.pageFour[i];}
     } else if (pageNumber === 5) {
         for (i = 0; i < 4; i++) {
-        answerText[i].innerHTML = answers.pageFive[i];}
+        answerText[i].textContent = answers.pageFive[i];}
     } else if (pageNumber === 6) {
         displayScorePage();
     }
